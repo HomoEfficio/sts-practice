b90271d3fbee55018203d5fd6d83755943039a6a
Oh Myung Woon <omwomw@sk.com>
2017-10-14 12:20:13 +0900

    CommandLineRunner 추가

--- src/main/java/com/example/demo/sts/StsPracticeApplication.java
+++ src/main/java/com/example/demo/sts/StsPracticeApplication.java
@@ -1,13 +1,22 @@
 package com.example.demo.sts;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class StsPracticeApplication {
+public class StsPracticeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StsPracticeApplication.class, args);	
+		System.out.println("Before Run Spring Boot Application");
+		SpringApplication.run(StsPracticeApplication.class, args);
+		System.out.println("After Run Spring Boot Application");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Thread.sleep(500);
+		System.out.println("Hello Spring Boot Command Line Runner!");
 	}
 }

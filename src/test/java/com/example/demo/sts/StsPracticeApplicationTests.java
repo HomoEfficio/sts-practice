70deee44bec5e6314e3204fec4c1981c7f3299c0
Oh Myung Woon <omwomw@sk.com>
2017-10-15 04:36:50 +0900

    Algebra 뺄셈 단위 테스트

--- src/test/java/com/example/demo/sts/StsPracticeApplicationTests.java
+++ src/test/java/com/example/demo/sts/StsPracticeApplicationTests.java
@@ -2,15 +2,26 @@ package com.example.demo.sts;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class StsPracticeApplicationTests {
 
+	@Autowired
+	private AlgebraService algebraService;
+	
 	@Test
 	public void contextLoads() {
+	}
+	
+	@Test
+	public void albebra_subtract_3_1() throws Exception {
+		assertThat(algebraService.subtract(3, 1)).isEqualTo(2);
 	}
 
 }

89930b2c8befb4d130804413f3675f2a7aba4357
Oh Myung Woon <omwomw@sk.com>
2017-10-14 13:59:49 +0900

    Algebra 서비스 추가

--- src/main/java/com/example/demo/sts/MyCommandLineRunner.java
+++ src/main/java/com/example/demo/sts/MyCommandLineRunner.java
@@ -1,14 +1,29 @@
 package com.example.demo.sts;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 @Component
 public class MyCommandLineRunner implements CommandLineRunner {
 
+	private final AlgebraService algebraService;
+	
+	@Value("${service.algebra.add}")
+	private String addLabel;
+	
+	@Autowired
+	public MyCommandLineRunner(AlgebraService algebraService) {
+		super();
+		this.algebraService = algebraService;
+	}
+
 	@Override
 	public void run(String... args) throws Exception {
 		System.out.println("My Command Line Runner!!");
+		int a = 3, b = 5;
+		System.out.println(addLabel + ": " + a + ", " + b + " = " + this.algebraService.add(3,  5));
 	}
 
 }

e6b524ae03354538aa8f203d3f3f9bd62d5f0059
Oh Myung Woon <omwomw@sk.com>
2017-10-14 12:58:10 +0900

    application.properties 값 읽어오기

--- src/main/java/com/example/demo/sts/StsPracticeApplication.java
+++ src/main/java/com/example/demo/sts/StsPracticeApplication.java
@@ -1,5 +1,6 @@
 package com.example.demo.sts;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,9 @@ import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class StsPracticeApplication implements CommandLineRunner {
+	
+	@Value("${myname}")
+	private String myName;
 
 	public static void main(String[] args) {
 		System.out.println("Before Run Spring Boot Application");
@@ -17,6 +21,6 @@ public class StsPracticeApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Thread.sleep(500);
-		System.out.println("Hello Spring Boot Command Line Runner!");
+		System.out.println("Hello Spring Boot Command Line Runner! by " + myName);
 	}
 }

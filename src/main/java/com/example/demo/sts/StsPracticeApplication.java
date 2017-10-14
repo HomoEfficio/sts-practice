a148af32b707c294ab5e13dd1d3c32d10b3bf631
Oh Myung Woon <omwomw@sk.com>
2017-10-14 12:14:16 +0900

    Initial Commit

---
+++ src/main/java/com/example/demo/sts/StsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.sts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class StsPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StsPracticeApplication.class, args);	
+	}
+}
